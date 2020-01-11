6001178c94ec4d3be53fb3d8b5a378971c26382d
aydinozhan <aydin.ozhan@yahoo.com>
2020-01-11 15:20:29 +0300

    Event model added

--- Models/Event.cs
+++ Models/Event.cs
@@ -1,36 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace net_core_bootcamp_b1_aydinozhan.Models
 {
     public class Event
     {
-
         public Guid Id { get; set; }
-
         public bool IsDeleted { get; set; }
-
         public DateTime CreatedAt { get; set; }
-
+        [Required, MaxLength(250)]
         public string Name { get; set; }
-
+        [Required]
         public DateTime StartDate { get; set; }
-
+        [Required]
         public DateTime FinishDate { get; set; }
-
         public string Address { get; set; }
-
         public bool IsFree { get; set; }
-
         public double Price { get; set; }
-
         public string Subject { get; set; }
-
         public string Description { get; set; }
-
-
-
     }
 }
