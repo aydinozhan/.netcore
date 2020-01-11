11b8ba557515931c72bfeeeab490694c434c521e
aydinozhan <aydin.ozhan@yahoo.com>
2020-01-11 11:21:57 +0300

    ders1

---
+++ Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace net_core_bootcamp_b1_aydinozhan
+{
+    public class Product
+    {
+
+        public Guid? Id { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public string Name { get; set; }
+        public string Desc { get; set; }
+        public double Price { get; set; }
+
+    }
+}
